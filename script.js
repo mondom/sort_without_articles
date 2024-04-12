3c6435f5744075251d23e70e10bdb6cb8d93a443
Monika <codingbymon@gmail.com>
2024-04-12 14:24:07 +0200

    Preparing to remove selected words from band table items

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh, Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog',
+]
+
+const word = 'The'
+
+const sortByAlphabeth = () => {
+	bands.forEach(item => {
+		const startIndex = item.indexOf(word)
+
+		if (startIndex !== -1) {
+			const endIndex = startIndex + word.length
+			const withoutThe = item.slice(0, startIndex) + item.slice(endIndex)
+            console.log(withoutThe);
+		}
+        
+	})
+}
+
+sortByAlphabeth()
