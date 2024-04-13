aa2f289a46a087f303272895ecbe5284327dc877
Monika <codingbymon@gmail.com>
2024-04-13 07:48:03 +0200

    finish

--- script.js
+++ script.js
@@ -14,10 +14,37 @@ const bands = [
 	'An Old Dog',
 ]
 
-function strip(bandName) {
-	return bandName.replace(/^(a |the |an )/i, '').trim()
+// function strip(bandName) {
+// 	return bandName.replace(/^(a |the |an )/i, '').trim()
+// }
+
+// const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1))
+
+// console.log(sortedBands);
+
+// document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('')
+
+// Definiujemy tablicę bands, która zawiera nazwy zespołów muzycznych.
+// Tworzymy funkcję strip, która usuwa z nazw zespołów określone przedrostki, takie jak "a", "the" lub "an". Wykorzystujemy do tego wyrażenie regularne /(^(a |the |an ))/i, które pasuje do tych przedrostków na początku ciągu znaków ignorując wielkość liter. Funkcja trim() usuwa dodatkowe białe znaki na początku i na końcu nazwy zespołu.
+// Sortujemy tablicę bands za pomocą metody sort(). Przekazujemy do niej funkcję porównującą, która porównuje nazwy zespołów po usunięciu przedrostków. Jeśli nazwa a powinna być przed b w porządku alfabetycznym, funkcja porównująca zwraca wartość większą od zera, w przeciwnym razie zwraca wartość mniejszą od zera.
+// Wybieramy element z dokumentu o identyfikatorze bands za pomocą document.querySelector('#bands') i ustawiamy jego innerHTML na listę posortowanych zespołów muzycznych. Do tego celu wykorzystujemy metodę map(), aby stworzyć listę elementów <li> zawierających nazwy zespołów, a następnie łączymy je w jedną długi ciąg znaków za pomocą metody join('').
+// Ogólnie rzecz biorąc, ten kod usuwa przedrostki "a", "the" lub "an" z nazw zespołów muzycznych, sortuje je alfabetycznie i wyświetla posortowaną listę na stronie internetowej.
+
+const sort = nameItem => {
+	return nameItem.replace(/^(a |the |an )/i, '').trim()
 }
+// pod nameItem jest podstawiana każda nazwa z tablicy bands(patrz wywołanie w metodzie sort poniżej) i słowa z wyrażenia regularnego są zastępowane pustym stringiem. Metoda trim usuwa białe znaki.
 
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1))
+const sortedBans = bands.sort((a, b) => {
+	if (sort(a) > sort(b)) {
+		return 1
+	} else {
+		return -1
+	}
+})
+// ↑ To jest sortowanie alfabetyczne tablicy. Wynik zwrócony większy od zera przesunie item w górę, a mniejszy od zera w dół.
+console.log(sortedBans)
 
-document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('')
+const list = document.querySelector('#bands')
+
+list.innerHTML = sortedBans.map(band => `<li>${band}</li>`).join('')
